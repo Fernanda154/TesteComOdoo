dd994332e730273e197bb0883194d8602a74efb2
Fernanda Guilherme <eufernandagui154@hotmail.com>
2021-08-03 09:26:56 -0300

    Adiciona campo name obrigatório.

--- odoo/addons/tarefa/models/models.py
+++ odoo/addons/tarefa/models/models.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 
 class tarefa(models.Model):
     _name = 'tarefa.tarefa'
-
+    name = fields.Char()
     responsavel = fields.Char()
     description = fields.Text()
 
